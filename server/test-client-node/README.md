56ca3c1193adf45bf120d695eb2b5c97fc44a6ec
Kaushik Chaubal <kaushikchaubal@gmail.com>
2020-04-21 11:56:41 +0100

    Working example of node.js client

---
+++ server/test-client-node/README.md
@@ -0,0 +1,14 @@
+1. Create an empty package.json file
+
+2. Convert this into a npm project
+    ```
+    npm init
+    ```
+
+3. Install the grpc & path package for node
+    ```
+    npm install grpc
+    npm install path
+    ```
+
+4
---
+++ server/test-client-node/app.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const path = require('path')
+const PROTO_PATH = path.join('defs', 'recipes-service.proto')
+console.log(PROTO_PATH);
+const SERVER_ADDR = 'localhost:50000'
+
+const grpc = require('grpc')
+var protoLoader = require('@grpc/proto-loader');
+var packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {keepCase: true,
+     longs: String,
+     enums: String,
+     defaults: true,
+     oneofs: true
+    });
+
+const recipes = grpc.loadPackageDefinition(packageDefinition).recipes;
+
+function main() {
+    const client = new recipes.RecipesService(SERVER_ADDR, grpc.credentials.createInsecure());
+    client.addRecipe({recipe_name: "Test"}, function(error, response) {
+        if(error) {
+            console.log(error);
+            return;
+        }
+
+        console.log(response);
+    });
+}
+
+main();
