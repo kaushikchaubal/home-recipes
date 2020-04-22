574c96433510d184125626d503c4678b38650aa9
Kaushik Chaubal <kaushikchaubal@gmail.com>
2020-04-22 11:03:50 +0100

    Wrapping with flatbuttons

--- client/home_recipes_app/lib/main.dart
+++ client/home_recipes_app/lib/main.dart
@@ -43,52 +43,58 @@ class HomeRecipesApp extends StatelessWidget {
                     color: Colors.teal.shade100,
                   ),
                 ),
-                Card(
-                    margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.fastfood,
-                        color: Colors.teal,
-                      ),
-                      title: Text(
-                        'Show all the recipes available',
-                        style: TextStyle(
-                          color: Colors.teal.shade900,
-                          fontFamily: 'Source Sans Pro',
-                          fontSize: 20.0,
+                FlatButton(
+                  child: Card(
+                      margin: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.fastfood,
+                          color: Colors.teal,
                         ),
-                      ),
-                    )),
-                Card(
-                    margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.kitchen,
-                        color: Colors.teal,
-                      ),
-                      title: Text(
-                        'List ingredients in your kitchen',
-                        style: TextStyle(
-                            fontSize: 20.0,
+                        title: Text(
+                          'Show all the recipes available',
+                          style: TextStyle(
                             color: Colors.teal.shade900,
-                            fontFamily: 'Source Sans Pro'),
-                      ),
-                    )),
-                Card(
-                    margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.cake,
-                        color: Colors.teal,
-                      ),
-                      title: Text(
-                        'Get ingredients for recipes',
-                        style: TextStyle(
+                            fontFamily: 'Source Sans Pro',
                             fontSize: 20.0,
-                            color: Colors.teal.shade900,
-                            fontFamily: 'Source Sans Pro'),
-                      ),
-                    ))
+                          ),
+                        ),
+                      )),
+                ),
+                FlatButton(
+                  child: Card(
+                      margin: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.kitchen,
+                          color: Colors.teal,
+                        ),
+                        title: Text(
+                          'List ingredients in your kitchen',
+                          style: TextStyle(
+                              fontSize: 20.0,
+                              color: Colors.teal.shade900,
+                              fontFamily: 'Source Sans Pro'),
+                        ),
+                      )),
+                ),
+                FlatButton(
+                  child: Card(
+                      margin: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.cake,
+                          color: Colors.teal,
+                        ),
+                        title: Text(
+                          'Get ingredients for recipes',
+                          style: TextStyle(
+                              fontSize: 20.0,
+                              color: Colors.teal.shade900,
+                              fontFamily: 'Source Sans Pro'),
+                        ),
+                      )),
+                )
               ],
             )),
         floatingActionButton: FloatingActionButton(
