8082dae29a110a9cd94f391e80c773e190adad0d
Kaushik Chaubal <kaushikchaubal@gmail.com>
2020-04-22 18:39:34 +0100

    Adding 3 newly added recipes

--- client/home_recipes_app/lib/GetIngredientsForRecipes.dart
+++ client/home_recipes_app/lib/GetIngredientsForRecipes.dart
@@ -9,12 +9,6 @@ class GetIngredientsForRecipes extends StatefulWidget {
 class _GetIngredientsForRecipesState extends State<GetIngredientsForRecipes> {
   int _selectedIndex = 0;
 
-  List<Widget> _mainWidgetOptions = <Widget>[
-    _getNewlyAdded(),
-    _getTrendingHigh(),
-    _getChefsSpecials(),
-  ];
-
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
@@ -28,9 +22,41 @@ class _GetIngredientsForRecipesState extends State<GetIngredientsForRecipes> {
         title: const Text('Get ingredients details'),
         backgroundColor: Colors.teal,
       ),
-      body: Center(
-        child: _mainWidgetOptions.elementAt(_selectedIndex),
-      ),
+      body: SingleChildScrollView(
+          child: Column(children: <Widget>[
+        _ingredientTitle('Croissants'),
+        _ingredientItem('500g strong white flour, plus extra for dusting'),
+        _ingredientItem('1½ tsp salt'),
+        _ingredientItem('50g sugar'),
+        _ingredientItem('2 x 7g sachets fast-action dried yeast'),
+        _ingredientItem('oil, for greasing'),
+        _ingredientItem('300g butter, at room temperature'),
+        _ingredientItem('1 egg, beaten'),
+        _ingredientTitle('Chicken pasta bake'),
+        _ingredientItem('4 tbsp olive oil'),
+        _ingredientItem('1 onion, finely chopped'),
+        _ingredientItem('2 garlic cloves, crushed'),
+        _ingredientItem('¼ tsp chilli flakes'),
+        _ingredientItem('2 x 400g cans chopped tomatoes'),
+        _ingredientItem('1 tsp caster sugar'),
+        _ingredientItem('6 tbsp mascarpone'),
+        _ingredientItem('4 skinless chicken breasts, sliced into strips'),
+        _ingredientItem('300g penne'),
+        _ingredientItem('70g mature cheddar, grated'),
+        _ingredientItem('50g grated mozzarella'),
+        _ingredientItem('½ small bunch of parsley, finely chopped'),
+        _ingredientTitle('Roast salmon with preserved lemon'),
+        _ingredientItem('40g preserved lemon, flesh and pith removed'),
+        _ingredientItem('100ml gin'),
+        _ingredientItem('1kg side organic farmed or wild salmon (tail end)'),
+        _ingredientItem('50g sea salt'),
+        _ingredientItem('50g golden caster sugar'),
+        _ingredientItem('1 tsp thyme leaves'),
+        _ingredientItem('1 tsp chilli flakes'),
+        _ingredientItem('½ small bunch dill, washed'),
+        _ingredientItem('30g preserved lemons, seeds removed'),
+        _ingredientItem('4 tbsp olive oil'),
+      ])),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
@@ -52,6 +78,45 @@ class _GetIngredientsForRecipesState extends State<GetIngredientsForRecipes> {
       ),
     );
   }
+
+  Widget _ingredientTitle(String title) {
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(
+          title,
+          style: TextStyle(
+              color: Colors.black, fontSize: 24.0, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+
+  Widget _ingredientItem(String item) {
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 16.0, bottom: 4.0),
+        child: Text(item),
+      ),
+    );
+  }
+
+  Widget _titleContainer(String myTitle) {
+    return Text(
+      myTitle,
+      style: TextStyle(
+          color: Colors.black, fontSize: 24.0, fontWeight: FontWeight.bold),
+    );
+  }
+
+  // TODO - fixme later
+  List<Widget> _mainWidgetOptions = <Widget>[
+    _getNewlyAdded(),
+    _getTrendingHigh(),
+    _getChefsSpecials(),
+  ];
 
   static Widget _getNewlyAdded() {
     return Text(
